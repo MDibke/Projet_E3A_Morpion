2156ab517a9205128bb02c1280ccac39f45a839d
Mathis DIBKE <mathis.dibke@reseau.eseo.fr>
2023-02-02 11:55:16 +0100

    Commantaire

--- Projet Morpion-20230105/etape1/board.c
+++ Projet Morpion-20230105/etape1/board.c
@@ -21,10 +21,12 @@ EndOfGameCallback endOfGame;
  * @return a boolean that tells if the game is finished
  */
 
+//Test la diagonale partant du haut gauche jusqu'au bas droit du plateau et retounr TRUE si il y a trois même signe sur cette diagonale
 bool diagonal1Test(const PieceType boardSquares[3][3], Coordinate CoordonateX, Coordinate CoordonateY)
 {
   bool status = false;
 
+  //test si les dernières coordonnées du points joué sont dans un des coin du plateau ou au centre
   if(((CoordonateX == 0 || CoordonateX == 2)&&(CoordonateY == 0 || CoordonateY == 2))||(CoordonateX == 1 && CoordonateY == 1))
   { 
     for(unsigned int ligneColomn = 0; ligneColomn < 3; ligneColomn++)
@@ -52,10 +54,12 @@ bool diagonal1Test(const PieceType boardSquares[3][3], Coordinate CoordonateX, C
   return status; 
 }
 
+//Test la diagonale partant du haut droit jusqu'au bas gauche du plateau et retounr TRUE si il y a trois même signe sur cette diagonale
 bool diagonal2Test(const PieceType boardSquares[3][3], Coordinate CoordonateX, Coordinate CoordonateY)
 {
   bool status = false;
 
+  //test si les dernières coordonnées du points joué sont dans un des coin du plateau ou au centre
   if(((CoordonateX == 0 || CoordonateX == 2)&&(CoordonateY == 0 || CoordonateY == 2))||(CoordonateX == 1 && CoordonateY == 1))
   { 
     for(unsigned int ligneColomn = 0; ligneColomn < 3; ligneColomn++)
@@ -83,6 +87,7 @@ bool diagonal2Test(const PieceType boardSquares[3][3], Coordinate CoordonateX, C
   return status; 
 }
 
+//Test la colonne du dernier point joué du plateau et retounr TRUE si il y a trois même signe sur cette colonne
 bool columnTest(const PieceType boardSquares[3][3], Coordinate CoordonateX, Coordinate CoordonateY)
 {
 	bool status = false;
@@ -114,6 +119,7 @@ bool columnTest(const PieceType boardSquares[3][3], Coordinate CoordonateX, Coor
 	return status;
 }
 
+//Test la ligne du dernier point joué du plateau et retounr TRUE si il y a trois même signe sur cette ligne
 bool lignTest(const PieceType boardSquares[3][3], Coordinate CoordonateX, Coordinate CoordonateY)
 {
 	bool status = false;
@@ -145,6 +151,7 @@ bool lignTest(const PieceType boardSquares[3][3], Coordinate CoordonateX, Coordi
 	return status;
 }
 
+//test si le plateau est plein et ne permet ps de rejouer
 bool fullTest(const PieceType boardSquares[3][3])
 {
   bool status = false;
@@ -171,6 +178,7 @@ bool fullTest(const PieceType boardSquares[3][3])
 static bool isGameFinished (const PieceType boardSquares[3][3], Coordinate lastChangeX, Coordinate lastChangeY, GameResult *gameResult)
 {
   bool returnGameResult = false,
+  //récupère les états des cases pouvant finir une partie par rapport au dernier point joué
   diagonalValue1 = diagonal1Test(boardSquares,lastChangeX,lastChangeY),
   diagonalValue2 = diagonal2Test(boardSquares,lastChangeX,lastChangeY),
   lignValue = lignTest(boardSquares,lastChangeX,lastChangeY),
@@ -179,14 +187,19 @@ static bool isGameFinished (const PieceType boardSquares[3][3], Coordinate lastC
 
   *gameResult = DRAW;
 
+  //vérifie si l'une des variable est égal à true
   if ((diagonalValue1 == true)||(diagonalValue2 == true)||(lignValue == true)||(columnValue == true))
   {
+    //retourne le gagnant à partir du dernier point joué
     if(boardSquares[lastChangeX][lastChangeY] == CIRCLE)
       *gameResult = CIRCLE_WINS;
     else if(boardSquares[lastChangeX][lastChangeY] == CROSS)
       *gameResult = CROSS_WINS;
+
+    //retourne que la partie est fini
     returnGameResult = true;
   }
+  //retourne que la partie est fini lorsque toute les cases de la grille est pleine et retounre comme gagnant DRAW
   else if (fullBoardValue == true)
   {
     returnGameResult = true;
