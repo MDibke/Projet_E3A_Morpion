c6f6e3bbea059600c532f71da12e05478b06ad26
Anthony <anthony.tapin@reseau.eseo.com>
2023-02-20 13:52:48 +0100

    finalisation Game.c et etape 2

--- Projet Morpion-20230105/etape2/player_manager_scanf.c
+++ Projet Morpion-20230105/etape2/player_manager_scanf.c
@@ -12,17 +12,44 @@
 
 #if defined CONFIG_PLAYER_MANAGER_SCANF
 
+PieceType player;
+
 void PlayerManager_init (void)
 {
+  player = CIRCLE;
 }
 
 void PlayerManager_free (void)
 {
+  player = NONE;
 }
 
 void PlayerManager_oneTurn (void)
 {
-  // TODO: à compléter
+  int x, y, read = 0;
+  bool ok = false;
+
+    // Affiche le qui joueur joue
+  BoardView_displayPlayersTurn(player);
+
+  do {
+        // Demande les coordonnées au format X,Y
+      printf("Place ta piece en X,Y:\n");
+      read = scanf("%d,%d", &x, &y);
+
+        // Si incorrect ou si la case est déjà prise,
+      if(read != 2 || x < 0 || x > 2 || y < 0 || y > 2 || Board_putPiece(x, y, player) != PIECE_IN_PLACE) {
+            // Vider le buffer et afficher un message d'erreur
+          char c;
+          while ((c = getchar()) != '\n' && c != EOF);
+          printf("\nPlacement  invalide...\n");
+        }
+        else ok = true;
+    }
+    while(!ok);
+
+    // Prochain tour
+    player = player == CIRCLE ? CROSS : CIRCLE;
 }
 
 #endif // defined CONFIG_PLAYER_MANAGER_SCANF
